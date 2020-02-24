8ca22c39467f7cac32e5418e951a54ae3e12addd
Shital <shital@Shital-PC.local>
2020-02-24 17:09:14 +0530

    task2

---
+++ array.js
@@ -0,0 +1,21 @@
+
+var array=[];
+
+function createArray()
+{
+    console.log(array);
+    return array;
+}
+
+function addElement()
+{
+    for(let i=0;i<=100;i++)
+        {
+            array.push(i)
+            
+        }
+        console.log(array);
+        
+}
+
+
