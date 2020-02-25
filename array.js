78ac2cf361f34af1b525d3f08f11a78735d33d71
Shital <shital@Shital-PC.local>
2020-02-25 12:24:32 +0530

    taskb

--- array.js
+++ array.js
@@ -20,4 +20,50 @@ function contentOfArray()
         
   }
 
+  function checkArray (numbers)
+    {
+        for (let num=0;num<=100;num++)
+        {
+          if (num%15===0)
+            {
+            console.log("FizzBuzz");
+            }
+          else if(num%3===0)
+            {
+            console.log("Fizz");
+            }
+          else if(num%5===0)
+            {
+            console.log("Buzz");
+            }
+            else
+            {
+                console.log(num);
+            }
+        }
+    }
+
+// function checkArray()
+// {
+//     const result=numbers.map(num=>{
+//         if (num%15===0)
+//         {
+//             console.log("BuzzFizz");
+//         }
+//         else if(num%5===0)
+//         {
+//             console.log("Buzz");
+//         }
+//         else if(num%3===0)
+//         {
+//             console.log("Fizz");
+//         }
+//         else 
+//         {
+//             console.log(num);
+//         }
+//     });
+    
+// }
+
 
