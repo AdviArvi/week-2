7c96814785ed032201d83bece3116732f8cc806a
Shital <shital@Shital-PC.local>
2020-02-26 11:15:31 +0530

    Test4

--- test4.js
+++ test4.js
@@ -1,19 +1,20 @@
 let button=document.getElementById('submit');
 
-let fname=document.getElementById("fname").value;
-let lname=document.getElementById("lname").value;
-let bdate=document.getElementById("bdate").value;
+let fname;
+let lname;
+let bdate;
 let users=[];
 
-//function for creating an object and insert into an array
+//function for creating an object and add into an array
 let addUser=()=>
 {      
     
     const person={
-        fname:document.getElementById("fname").value,
-        lname:document.getElementById("lname").value,
-        bdate:document.getElementById("bdate").value,
-    };
+                 fname:document.getElementById("fname").value,
+                 lname:document.getElementById("lname").value,
+                 bdate:document.getElementById("bdate").value,
+                 };
+
     users.push(person);
     console.log(users);
 
